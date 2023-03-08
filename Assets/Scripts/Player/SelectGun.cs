9f6f64e5ed157a522410a655f9b27734944222b8
bagelhamster <82683493+bagelhamster@users.noreply.github.com>
2023-03-08 02:57:08 +0000

    Added Rifle and custom crosshairs
    Added a rifle to the game along with unique crosshairs for each weapon that fit the goal for the weapon

--- Assets/Scripts/Player/SelectGun.cs
+++ Assets/Scripts/Player/SelectGun.cs
@@ -6,13 +6,26 @@ public class SelectGun : MonoBehaviour
 {
     public GameObject Gun1;
     public GameObject Gun2;
-    public GameObject Gun1Ammo;    public GameObject Gun2Ammo;
+    public GameObject Gun3;
+    public GameObject Gun1Ammo;
+    public GameObject Gun2Ammo;
+    public GameObject Gun3Ammo;
+    public GameObject Gun1Cross;
+    public GameObject Gun2Cross;
+    public GameObject Gun3Cross;
+
     void Start()
     {
         Gun1.gameObject.SetActive(true);
         Gun1Ammo.gameObject.SetActive(true);
         Gun2.gameObject.SetActive(false);
         Gun2Ammo.gameObject.SetActive(false);
+        Gun3.gameObject.SetActive(false);
+        Gun3Ammo.gameObject.SetActive(false);
+        Gun1Cross.gameObject.SetActive(true);
+        Gun2Cross.gameObject.SetActive(false);
+        Gun3Cross.gameObject.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -24,6 +37,11 @@ public class SelectGun : MonoBehaviour
             Gun1Ammo.gameObject.SetActive(true);
             Gun2.gameObject.SetActive(false);
             Gun2Ammo.gameObject.SetActive(false);
+            Gun3.gameObject.SetActive(false);
+            Gun3Ammo.gameObject.SetActive(false);
+            Gun1Cross.gameObject.SetActive(true);
+            Gun2Cross.gameObject.SetActive(false);
+            Gun3Cross.gameObject.SetActive(false);
 
         }
         if (Input.GetButton("Slot2"))
@@ -32,6 +50,23 @@ public class SelectGun : MonoBehaviour
             Gun1Ammo.gameObject.SetActive(false);
             Gun2.gameObject.SetActive(true);
             Gun2Ammo.gameObject.SetActive(true);
+            Gun3.gameObject.SetActive(false);
+            Gun3Ammo.gameObject.SetActive(false);
+            Gun1Cross.gameObject.SetActive(false);
+            Gun2Cross.gameObject.SetActive(true);
+            Gun3Cross.gameObject.SetActive(false);
+        }
+        if (Input.GetButton("Slot3"))
+        {
+            Gun1.gameObject.SetActive(false);
+            Gun1Ammo.gameObject.SetActive(false);
+            Gun2.gameObject.SetActive(false);
+            Gun2Ammo.gameObject.SetActive(false);
+            Gun3.gameObject.SetActive(true);
+            Gun3Ammo.gameObject.SetActive(true);
+            Gun1Cross.gameObject.SetActive(false);
+            Gun2Cross.gameObject.SetActive(false);
+            Gun3Cross.gameObject.SetActive(true);
 
         }
     }
